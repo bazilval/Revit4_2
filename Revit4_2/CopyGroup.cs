64f01a7cc53a4cd28b5ab5acbc3dbd285954e6c0
bazilval <bazilval@bk.ru>
2022-02-15 17:31:32 +0300

    - Добавлена фильтрация выбираемого объекта - Добавлено определение точки вставки относительно центра комнаты

--- Revit4_2/CopyGroup.cs
+++ Revit4_2/CopyGroup.cs
@@ -1,6 +1,8 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,26 +16,91 @@ namespace Revit4_2
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
-            Document doc = uiDoc.Document;
-
-            Reference refer = uiDoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, "Выберите группу");
-            Element element = doc.GetElement(refer);
-
-            var group = element as Group;
-
-            XYZ point = uiDoc.Selection.PickPoint("Укажите точку вставки");
-
-            using (var ts = new Transaction(doc, "Copying of group"))
+            try
             {
-                ts.Start();
+                UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+                Document doc = uiDoc.Document;
 
-                doc.Create.PlaceGroup(point, group.GroupType);
+                Reference refer = uiDoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, new GroupFilter(), "Выберите группу");
+                Element element = doc.GetElement(refer);
+                var group = element as Group;
 
-                ts.Commit();
+                XYZ groupCenter = GetElementCenter(group);
+                Room groupRoom = GetRoomByPoint(doc, groupCenter);
+                XYZ roomCenter = GetElementCenter(groupRoom);
+                XYZ offset = groupCenter - roomCenter;
+
+                XYZ userPoint = uiDoc.Selection.PickPoint("Укажите точку вставки");
+                Room newRoom = GetRoomByPoint(doc, userPoint);
+                XYZ point;
+                if (newRoom != null)
+                {
+                    point = offset + GetElementCenter(newRoom);
+                }
+                else
+                {
+                    point = userPoint;
+                }
+
+                using (var ts = new Transaction(doc, "Copying of group"))
+                {
+                    ts.Start();
+
+                    doc.Create.PlaceGroup(point, group.GroupType);
+
+                    ts.Commit();
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;                
             }
 
-            return Result.Succeeded;
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;                
+            }
+
+        }
+
+        public XYZ GetElementCenter(Element elem)
+        {
+            BoundingBoxXYZ boundingBox = elem.get_BoundingBox(null);
+            return (boundingBox.Min + boundingBox.Max) / 2;
+        }
+
+        public Room GetRoomByPoint(Document doc, XYZ point)
+        {
+            var collector = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (var elem in collector)
+            {
+                var room = elem as Room;
+                if (room != null)
+                {
+                    if (room.IsPointInRoom(point))
+                    {
+                        return room;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+
+    public class GroupFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Group;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
         }
     }
 }
